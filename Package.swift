682a7f5d8208360037afa118c08d05e7c007136a
Damian Malarczyk <damian.malarczyk@gmail.com>
2016-12-09 19:45:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "JSONNode",
     dependencies: [
-	.Package(url: "https://github.com/vdka/JSON.git", Version(0,16,1)),
+	.Package(url: "https://github.com/vdka/JSON.git", majorVersion: 0, minor: 16),
 	.Package(url: "https://github.com/vapor/node.git", majorVersion: 1),
 	.Package(url: "https://github.com/vapor/core.git", majorVersion: 1)
     ]
